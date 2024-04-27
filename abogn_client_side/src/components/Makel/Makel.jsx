737dd69d13e66e71902e43d1acf7268175e27ef6
nowyouseeme1234 <issacabreham96@gmail.com>
2024-04-27 10:33:21 +0300

    implemented add and deduct socket

--- abogn_client_side/src/components/Makel/Makel.jsx
+++ abogn_client_side/src/components/Makel/Makel.jsx
@@ -1,30 +1,126 @@
 import React from 'react'
 import { IoIosArrowBack } from "react-icons/io";
+import { IoMdAdd } from "react-icons/io";
+import { RiSubtractFill } from "react-icons/ri";
+import { useState, useEffect } from 'react';
+import { IoIosCloseCircle } from "react-icons/io";
+import { useRef } from 'react';
+import axios from 'axios';
 
-const itemList = [
-  {
-    id: 1,
-    title: 'Socket',
-  },
-  {
-    id: 2,
-    title: 'Scatola',
-  },
-  {
-    id: 3,
-    title: 'Switch',
-  },
-]
+
 const Makel = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState('');
+  const [modalStyle, setModalStyle] = useState('');
+  const [quantity, setQuantity] = useState(0);
+  const [inStock, setInStock] = useState('');
+  const [outStock, setOutStock] = useState('');
+  const modalRef = useRef();
+  let total = 0;
+  let out = 0;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/auth/get_item');
+        if(response.data.Status){
+          setInStock(response.data.Value.in_stock)
+          setOutStock(response.data.Value.out_stock)
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  });
+
+  const openModal = (content, style) => {
+    setIsModalOpen(true);
+    setModalContent(content);
+    setModalStyle(style);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+
+  };
+
+  const handleOutsideClick = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      closeModal();
+    }
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    total = inStock + quantity;
+    axios.put('http://localhost:3000/auth/add_item', { total }).then(result => {
+      if (result.data.Status) {
+        closeModal()
+      }
+      else {
+        console.log(result.data.Error)
+      }
+    }).catch(err => console.error(err))
+  }
+  const handleDeduct = (e) => {
+    e.preventDefault();
+    total = inStock - quantity;
+    out = outStock + quantity
+    axios.put('http://localhost:3000/auth/deduct_item', { out, total }).then(result => {
+      if (result.data.Status) {
+        closeModal()
+      }
+      else {
+        console.log(result.data.Error)
+      }
+    }).catch(err => console.error(err))
+  }
   return (
-    <div >
+    <div onClick={handleOutsideClick}>
+
       <div className='flex justify-start items-center gap-2'>
-      <IoIosArrowBack className='text-lg font-bold'/>
+        <IoIosArrowBack className='text-lg font-bold' />
         <h1 className='text-3xl font-[poppins] font-extrabold '>Makel</h1>
       </div>
-      <div>
 
+      <div className='bg-secondary flex justify-center items-center rounded-3xl'>
+        <div className='h-screen flex justify-start items-center'>
+          <div className='w-40 h-28 bg-gray-200 rounded-xl flex'>
+
+            <div>
+              <h1 className='text-xl font-extrabold font-[poppins] p-2'>Socket</h1>
+              <h3 className='text-green-800 text-end font-bold text-xs font-[poppins] mt-1'>IN-STOCK: <span className='text-black text-end'>{inStock}</span></h3>
+              <h3 className='text-red-800 text-end font-bold text-xs font-[poppins]'>OUT-STOCK: <span className='text-black'>{outStock}</span></h3>
+            </div>
+
+            <div className='flex flex-col items-end justify-center ml-6 gap-3'>
+              <button className=' p-3 bg-green-800 text-white rounded-md' onClick={() => openModal('Add', 'bg-green-800')}><IoMdAdd /></button>
+              <button className=' p-3 bg-red-800 text-black rounded-md' onClick={() => openModal('Deduct', 'bg-red-600')}><RiSubtractFill /></button>
+            </div>
+
+          </div>
+        </div>
       </div>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-6 rounded shadow" ref={modalRef}>
+            <button
+              className="text-2xl text-red-800 rounded float-right -mt-5 -mr-5"
+              onClick={closeModal}
+            >
+              <IoIosCloseCircle />
+            </button>
+            <h2 className="text-xl font-bold mb-3">Socket</h2>
+            <p>45114 surface phone socket</p>
+            <div className='flex justify-center items-center mt-4'>
+              <input type='number' className='border-2 rounded-lg w-16' value={quantity} onChange={(e) => { setQuantity(Number(e.target.value)) }} />
+              <button className={`${modalStyle} text-sm text-white px-1 py-1 rounded ml-2`} onClick={modalContent==='Add'? handleAdd : handleDeduct}>{modalContent}</button>
+            </div>
+          </div>
+        </div>
+      )}
+
     </div>
   )
 }
--- server/Routes/AdminRoute.js
+++ server/Routes/AdminRoute.js
@@ -18,5 +18,34 @@ router.post('/adminlogin', (req, res) => {
         }
     })
 })
+router.get('/get_item', (req, res) => {
+    const sql = "SELECT in_stock, out_stock FROM 45114_surface_phone_socket JOIN socket ON 45114_surface_phone_socket.id = socket.45114_surface_phone_socket_id JOIN makel ON makel.socket_id = socket.id JOIN items ON items.makel_id = makel.id WHERE 45114_surface_phone_socket.id = 1;"
+    con.query(sql, (err, result) => {
+        if(result.length>0){
+            return res.json({Status: true, Value: result[0]})
+        }
+        else{
+            return res.json({Status: false, Error: `${err}`})
+        }
+    })
+})
+
+router.put('/add_item', (req, res) =>{
+    const value = req.body.total;
+    const sql = "UPDATE 45114_surface_phone_socket set in_stock = ? where id = 1"
+    con.query(sql, value, (err, result) => {
+        if(err) return res.json({Status: false, Error: `${err}`})
+        return res.json({Status: true, Result: result})
+    })
+})
+router.put('/deduct_item', (req, res) =>{
+    const value = [req.body.out, req.body.total];
+    console.log(value)
+    const sql = "UPDATE 45114_surface_phone_socket set out_stock = ?, in_stock = ? where id = 1"
+    con.query(sql, [...value], (err, result) => {
+        if(err) return res.json({Status: false, Error: `${err}`})
+        return res.json({Status: true, Result: result})
+    })
+})
 
 export {router as adminRouter}
